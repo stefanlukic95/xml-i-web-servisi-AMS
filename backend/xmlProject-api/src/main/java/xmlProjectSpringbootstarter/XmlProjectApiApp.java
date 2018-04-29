9ed7ab7b99e2ca06cb371347481e687b1d8600ac
Stefan Lukic <stefan.lukic@yahoo.com>
2018-04-29 18:30:01 +0200

    inicijalizacija projekta

---
+++ backend/xmlProject-api/src/main/java/xmlProjectSpringbootstarter/XmlProjectApiApp.java
@@ -0,0 +1,15 @@
+package xmlProjectSpringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlProjectApiApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(XmlProjectApiApp.class, args);
+		
+	}
+
+}
