f3423e02eb60f6c69beebb20af9095c36ec4ac97
Stefan Lukic <stefan.lukic@yahoo.com>
2018-05-11 15:36:22 +0200

    pocetak

--- administrator/administrator/src/app/app.module.ts
+++ administrator/administrator/src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { CommonModule } from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
