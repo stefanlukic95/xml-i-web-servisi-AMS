641082666f93b48e202d702d20d5315237260018
Aleksandar <aleksandar91lazarevic@gmail.com>
2018-06-23 19:45:17 +0200

    poruka

---
+++ agent/XML_Agent/XML_Agent/Controllers/SmestajSOAPController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.ServiceModel;
+using System.Web.Http;
+using XML_Agent.SmestajSOAP;
+
+namespace XML_Agent.Controllers
+{
+    public class SmestajSOAPController : ApiController
+    {
+        private static SmestajSOAP ss;
+        private getTipoviRequest gtr;
+
+        BasicHttpBinding basic = new BasicHttpBinding();
+        InstanceContext ic = new InstanceContext(new SmestajSOAP());
+        SmestajServicePortClient p = new SmestajServicePortClient(new InstanceContext(new SmestajSOAP()), "SmestajServicePortSoap11", "SmestajServicePortSoap11");
+
+
+
+        public SmestajSOAPController()
+        {
+            ss = new SmestajSOAP();
+            gtr = new getTipoviRequest();
+            gtr.name = "aaa";
+
+
+
+        }
+
+        [OperationContract]
+        public HttpResponseMessage GetAll()
+        {
+            var smestaj = p.getTipovi(gtr);
+
+            return Request.CreateResponse(HttpStatusCode.OK, smestaj);
+
+        }
+    }
+}
---
+++ agent/XML_Agent/XML_Agent/Services/SmestajSOAP.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using XML_Agent.SmestajSOAP;
+
+namespace XML_Agent.Services
+{
+    public class SmestajSOAP : SmestajServicePortCallback
+    {
+        public void getCountry(getCountryResponse1 request)
+        {
+            Console.WriteLine("adsadas");
+        }
+    }
+}
